7e82fce67e57c8cb8d39391ca1e1496f4674e683
elene638 <elenejavaxishvili@yahoo.com>
2023-11-06 06:31:38 -0800

    feat: handle checkout page

--- client/src/pages/Checkout.tsx
+++ client/src/pages/Checkout.tsx
@@ -1,10 +1,44 @@
-import React from 'react'
+import { useContext } from "react"
+import { useGetProducts } from "../hooks/useGetProducts"
+import { IProduct } from "../models/interfaces"
+import { ShopContext, ShopContextI } from "../context/shop-context"
+import CartItem from "../components/CartItem"
+import "./Checkout.css"
+import { useNavigate } from "react-router"
 
-type Props = {}
+const Checkout = () => {
+    const { products } = useGetProducts()
 
-const Checkout = (props: Props) => {
+    const { getCartItemCount, getTotalCartAmount, checkout } = useContext<ShopContextI>(ShopContext)
+
+    const navigate = useNavigate()
+
+    const totalAmount = getTotalCartAmount()
     return (
-        <div>Checkout</div>
+        <div className="checkout-wrapper">
+            <h3>Your cart items</h3>
+            <div className="products">
+                {products?.map((product: IProduct) => {
+                    if (getCartItemCount(product._id) !== 0) {
+                        return <CartItem product={product} key={product._id} />
+                    }
+                })}
+            </div>
+            {totalAmount > 0 ? (
+                <div className="checkout-container">
+                    <h3>Subtotal: {totalAmount.toFixed(2)} $</h3>
+                    <div className="button-cont">
+                        <button onClick={() => navigate("/")}>Continue shopping</button>
+                        <button onClick={checkout}>Checkout</button>
+                    </div>
+                </div>
+            ) : (
+                <>
+                    <h3>Your shopping Cart is empty</h3>
+                    <button onClick={() => navigate("/")}>Continue shopping</button>
+                </>
+            )}
+        </div>
     )
 }
 
